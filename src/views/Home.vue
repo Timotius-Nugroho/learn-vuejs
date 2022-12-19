ab4335403d6ab9d13f676d9a35e6597b9b601241
Timotius Nugroho <timotius.nugroho@aruna.id>
2022-12-19 12:19:18 +0700

    add vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,29 @@
   <div>
     <h1>HOME....</h1>
     <HelloWorld msg="Timotius" alias="Tims " />
+    <div :style="{ marginTop: '20px', color: $store.state.colorCode }">
+      Init: {{ this.$store.state.count }} - Sqrt:
+      {{ this.$store.getters.counterSqrt }}
+    </div>
+    <div>
+      <button
+        :style="{ padding: '10px', margin: '2px', backgroundColor: 'aqua' }"
+        @click="$store.dispatch('inc')"
+      >
+        +
+      </button>
+      <button
+        :style="{ padding: '10px', margin: '2px', backgroundColor: 'aqua' }"
+        @click="$store.commit('dec')"
+      >
+        -
+      </button>
+    </div>
+    <input
+      v-model="$store.state.colorCode"
+      type="text"
+      placeholder="NEW COLOR"
+    />
   </div>
 </template>
 
@@ -13,5 +36,15 @@ export default {
   components: {
     HelloWorld,
   },
+  // computed: {
+  //   colorCode: {
+  //     get() {
+  //       return this.$store.state.colorCode;
+  //     },
+  //     set(newValue) {
+  //       this.$store.commit("setColorCode", newValue);
+  //     },
+  //   },
+  // },
 };
 </script>
